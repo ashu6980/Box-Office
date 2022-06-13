1ddaec489063de00b8c6777ef9bddad9e9529bd3
Ashu <akkiashu680537@gamil.com>
2022-06-13 13:01:49 +0530

    created custom hooks for show pages

---
+++ src/misc/custom-hooks.js
@@ -0,0 +1,30 @@
+import { useEffect, useReducer } from 'react';
+
+function showsReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.showId];
+    }
+    case 'REMOVE': {
+      return prevState.filter(showId => showId !== action.showId);
+    }
+
+    default:
+      return prevState;
+  }
+}
+
+function userPersistedReducer(reducer, initalState, key) {
+  const [state, dispatch] = useReducer(reducer, initalState, initial => {
+    const persisted = localStorage.getItem(key);
+    return persisted ? JSON.parse(persisted) : initial;
+  });
+  useEffect(() => {
+    localStorage.getItem(key, JSON.stringify(state));
+  }, [state, key]);
+  return [state, dispatch];
+}
+
+export function useShows(key = 'shows') {
+  return userPersistedReducer(showsReducer, [], key);
+}
